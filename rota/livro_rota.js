76859393417a166dc7e8da4be38ca7f5ab50ca65
luam feijo <dev03@sulsystem.com>
2023-05-30 15:28:01 -0300

    minor details

--- rota/livro_rota.js
+++ rota/livro_rota.js
@@ -4,8 +4,8 @@ const livroController = require("../controller/livro_controller")
 const router = express.Router()
 
 //api/livros
-router.post('/',  livroController.inserir)
-router.get('/',livroController.listar)
+router.post('/', livroController.inserir)
+router.get('/', livroController.listar)
 router.get('/:id', livroController.buscarPorId)
 router.put('/:id', livroController.atualizar)
 router.delete('/:id', livroController.deletar)
