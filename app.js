482615619c59080a88e9b61c24c7918d9d4ca8f8
Luam Ahrion <luam_azf@hotmail.com>
2023-07-05 23:22:18 -0300

    pacote cors instalado

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const livroRota = require('./rota/livro_rota')
+const cors = require('cors');
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(cors());
 
 app.use("/api/livros", livroRota)
 
